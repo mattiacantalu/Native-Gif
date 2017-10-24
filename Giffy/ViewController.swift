9c271c28d7499db788c02dc8e3abd63e6ac72a13
Mattia Cantalù <mattia.cantalu.external@ynap.com>
2017-10-24 14:16:12 +0200

    first commit

--- Giffy/ViewController.swift
+++ Giffy/ViewController.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageVIew: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let gif1 = UIImage(named: "Tackle-1.gif")!
+        let gif2 = UIImage(named: "Tackle-2.gif")!
+        let gif3 = UIImage(named: "Tackle-3.gif")!
+        let gif4 = UIImage(named: "Tackle-4.gif")!
+        let gif5 = UIImage(named: "Tackle-5.gif")!
+        let gif6 = UIImage(named: "Tackle-6.gif")!
+
+        imageVIew.animationImages = [gif1,
+                                     gif2,
+                                     gif3,
+                                     gif4,
+                                     gif5,
+                                     gif6
+        ]
+        imageVIew.animationDuration = 1.0
+        imageVIew.animationRepeatCount = 0
+        imageVIew.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
